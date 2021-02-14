591a672cd1982a4c4232d2369cc907536bdf405f
cedric-joaquin <joaquin.cedric@gmail.com>
2021-02-14 12:30:03 -0800

    CRUD controllers

--- copify-api/app/controllers/items_controller.rb
+++ copify-api/app/controllers/items_controller.rb
@@ -1,2 +1,36 @@
 class ItemsController < ApplicationController
+
+    def index
+        inventory = Item.all
+        render json: inventory
+    end
+
+    def create
+        item = Item.create(
+            purchase_date: params[:purchase_date],
+            name: params[:name],
+            brand: params[:brand],
+            size: params[:size],
+            cost: params[:cost]
+        )
+        render json: item
+    end
+
+    def update
+        item = Item.find_by(id: params[:id])
+        item.update(
+            purchase_date: params[:purchase_date],
+            name: params[:name],
+            brand: params[:brand],
+            size: params[:size],
+            cost: params[:cost]
+        )
+        render json: item
+    end
+
+    def destroy
+        item = Item.find_by(id: params:id])
+        item.destroy
+    end
+
 end
