b26738889f8c6837cf73ded26ddd55b9ec13e921
cedric-joaquin <joaquin.cedric@gmail.com>
2021-02-14 13:24:25 -0800

    create components and containers for inventory

---
+++ src/containers/ItemsContainer.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Items from '../components/items/Items';
+
+export default class ItemsContainer extends Component {
+    render() {
+        return (
+            <Items></Items>
+        )
+    }
+}
