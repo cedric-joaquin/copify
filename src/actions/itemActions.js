170a67abc88c05928a2150ed50026393c7e87699
cedric-joaquin <joaquin.cedric@gmail.com>
2021-02-15 10:47:15 -0800

    add item action

--- src/actions/itemActions.js
+++ src/actions/itemActions.js
@@ -3,6 +3,12 @@ export const fetchItems = () => {
         dispatch({ type: 'LOADING_INVENTORY'})
         fetch('http://localhost:4000/items')
             .then(resp => resp.json())
-            .then(items => dispatch({type:'ADD_ITEMS', items}))
+            .then(items => dispatch({type:'LOAD_INVENTORY', items}))
+    }
+}
+
+export const addItem = (item) => {
+    return (dispatch) => {
+        dispatch({type: 'ADD_ITEM', item})
     }
 }
--- src/reducers/itemsReducer.js
+++ src/reducers/itemsReducer.js
@@ -11,7 +11,7 @@ const itemsReducer = (state = {items: [], loading:false}, action) => {
                 loading: true
             }
 
-        case 'ADD_ITEMS':
+        case 'LOAD_INVENTORY':
             return {
                 ...state,
                 items: action.items,
