6e6059451bbb427c86be8f62edbc99db68520875
cedric-joaquin <joaquin.cedric@gmail.com>
2021-02-20 14:44:52 -0800

    show items as table

--- src/components/items/Items.js
+++ src/components/items/Items.js
@@ -4,12 +4,30 @@ import Item from './Item.js';
 export default class Items extends Component {
 
     render() {
-        const items = this.props.items.map(item => <Item item={item} deleteItem={this.props.deleteItem} editItem={this.props.editItem}></Item>)
+        const items = this.props.items.map(item => {
+                return (
+                    <tr>
+                        <td>{item.purchase_date}</td>
+                        <td>{item.name}</td>
+                        <td>{item.cost}</td>
+                    </tr>
+                )
+                // <Item item={item} key={item.id} deleteItem={this.props.deleteItem} editItem={this.props.editItem}></Item>
+            })
         
         return (
-            <div>
-                {items}
-            </div>
+            <table class="table table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <th scope="col">Purchase Date</th>
+                        <th scope="col">Item</th>
+                        <th scope="col">Cost</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {items}
+                </tbody>
+            </table>
         )
     }
 }
