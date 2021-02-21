6e8e8b8c38a2d1f94b6fef95397e762e55a4e848
cedric-joaquin <joaquin.cedric@gmail.com>
2021-02-20 16:08:22 -0800

    pass in proper dispatches

--- src/components/items/Items.js
+++ src/components/items/Items.js
@@ -31,7 +31,7 @@ export default class Items extends Component {
                         {items}
                     </tbody>
                 </table>
-                <Route path={'/inventory/:itemId'} render={routerProps => <Item {...routerProps} items={this.props.items} /> }/>
+                <Route path={'/inventory/:itemId'} render={routerProps => <Item {...routerProps} items={this.props.items} deleteItem={this.props.deleteItem} addItem={this.props.addItem}/> }/>
             </div>
         )
     }
